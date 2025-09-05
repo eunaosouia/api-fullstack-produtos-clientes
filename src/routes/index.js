9fddf14b57f6aa510e4a5ba7d36030cbddc5ec02
Felipe Santos <gestorfelipgs@gmail.com>
2025-09-05 17:19:19 -0300

    feat: Express server created

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes');
+
+const produtoController = require('./controllers/produtoController');
+const clienteController = require('./controllers/clienteController');
+
+routes.post('/produtos', produtoController.create);
+routes.get('/produtos', produtoController.read);
+routes.get('/produtos/:id', produtoController.readById);
+
+routes.post('/clientes', clienteController.create);
+routes.get('/clientes', clienteController.read);
+routes.get('/clientes/:id', clienteController.readById);
+
+module.exports = routes;
